cb46e549b5a6e0cb9f3d293d29aa3e9a85c94f05
chaimpan <chaim@chaimpaneth.com>
2018-08-12 20:23:27 +0300

    delegate fix

--- ios/RNJWPlayerDelegateProxy.h
+++ ios/RNJWPlayerDelegateProxy.h
@@ -5,11 +5,7 @@
 @class RNJWPlayerNativeView;
 
 @interface RNJWPlayerDelegateProxy : NSObject<JWPlayerDelegate>
+
 @property(nonatomic, strong)RNJWPlayerNativeView *delegate;
-- (void)onBeforePlay;
-- (void)onBuffer:(JWEvent<JWBufferEvent> *)event;
-- (void)onPlay:(JWEvent<JWStateChangeEvent> *)event;
-- (void)onTime:(JWEvent<JWTimeEvent> *)event;
-- (void)onPause:(JWEvent<JWStateChangeEvent> *)event;
-- (void)onError:(NSError *)error;
+
 @end
