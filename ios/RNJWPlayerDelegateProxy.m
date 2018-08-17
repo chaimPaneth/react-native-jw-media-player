29fe1e67094a986e7ace65e4da7d16b71de037ff
Chaim Paneth <caapaneth@gmail.com>
2018-08-17 14:21:13 +0300

    android init, params add to js

--- ios/RNJWPlayerDelegateProxy.m
+++ ios/RNJWPlayerDelegateProxy.m
@@ -3,32 +3,49 @@
 @implementation RNJWPlayerDelegateProxy
 #pragma mark - RNJWPlayer Delegate
 
-- (void)onBeforePlay {
+- (void)onBeforePlay
+{
     [self.delegate onRNJWPlayerBeforePlay];
 }
 
-- (void)onBuffer:(JWEvent<JWBufferEvent> *)event {
+- (void)onBuffer:(JWEvent<JWBufferEvent> *)event
+{
     [self.delegate onRNJWPlayerBuffer];
 }
 
-- (void)onPlay:(JWEvent<JWStateChangeEvent> *)event {
+- (void)onPlay:(JWEvent<JWStateChangeEvent> *)event
+{
     [self.delegate onRNJWPlayerPlay];
 }
 
-- (void)onTime:(JWEvent<JWTimeEvent> *)event {
+- (void)onTime:(JWEvent<JWTimeEvent> *)event
+{
     [self.delegate onRNJWPlayerTime:event];
 }
 
-- (void)onPause:(JWEvent<JWStateChangeEvent> *)event {
+- (void)onPause:(JWEvent<JWStateChangeEvent> *)event
+{
     [self.delegate onRNJWPlayerPause];
 }
 
-- (void)onSetupError:(JWEvent<JWErrorEvent> *)event {
+- (void)onSetupError:(JWEvent<JWErrorEvent> *)event
+{
     [self.delegate onRNJWSetupPlayerError:event];
 }
 
-- (void)onError:(JWEvent<JWErrorEvent> *)event {
+- (void)onError:(JWEvent<JWErrorEvent> *)event
+{
     [self.delegate onRNJWSetupPlayerError:event];
+}
+
+- (void)onFullscreen:(JWEvent<JWFullscreenEvent> *)event
+{
+    [self.delegate onRNJWFullScreen:event];
+}
+
+- (void)onFullscreenRequested:(JWEvent<JWFullscreenEvent> *)event
+{
+    [self.delegate onRNJWFullScreenRequested:event];
 }
 
 @end
--- ios/RNJWPlayerNativeView.h
+++ ios/RNJWPlayerNativeView.h
@@ -6,12 +6,21 @@
 @class RNJWPlayerDelegateProxy;
 
 @interface RNJWPlayerNativeView : UIView
+
 @property(nonatomic, strong)JWPlayerController *player;
 @property(nonatomic, strong)RNJWPlayerDelegateProxy *proxy;
+
 @property(nonatomic, strong)NSString *file;
 @property(nonatomic)BOOL autostart;
 @property(nonatomic)BOOL controls;
 @property(nonatomic)BOOL repeat;
+@property(nonatomic)BOOL displayTitle;
+@property(nonatomic)BOOL displayDesc;
+@property(nonatomic, strong)NSString *title;
+@property(nonatomic, strong)NSString *image;
+@property(nonatomic, strong)NSString *desc;
+@property(nonatomic, strong)NSString *mediaId;
+
 @property(nonatomic, copy)RCTBubblingEventBlock onBeforePlay;
 @property(nonatomic, copy)RCTBubblingEventBlock onPlay;
 @property(nonatomic, copy)RCTBubblingEventBlock onPause;
@@ -19,6 +28,9 @@
 @property(nonatomic, copy)RCTBubblingEventBlock onSetupPlayerError;
 @property(nonatomic, copy)RCTBubblingEventBlock onPlayerError;
 @property(nonatomic, copy)RCTBubblingEventBlock onTime;
+@property(nonatomic, copy)RCTBubblingEventBlock onFullScreen;
+@property(nonatomic, copy)RCTBubblingEventBlock onFullScreenRequested;
+
 -(void)onRNJWPlayerBeforePlay;
 -(void)onRNJWPlayerPlay;
 -(void)onRNJWPlayerPause;
@@ -26,4 +38,7 @@
 -(void)onRNJWSetupPlayerError:(JWEvent<JWErrorEvent> *)event;
 -(void)onRNJWPlayerError:(JWEvent<JWErrorEvent> *)event;
 -(void)onRNJWPlayerTime:(JWEvent<JWTimeEvent> *)event;
+-(void)onRNJWFullScreen:(JWEvent<JWFullscreenEvent> *)event;
+-(void)onRNJWFullScreenRequested:(JWEvent<JWFullscreenEvent> *)event;
+
 @end
