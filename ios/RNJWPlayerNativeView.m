1ab081a7d0b9221d809dc1f8bfdd20f2569ae1e0
shubh4solanki <>
2019-03-08 11:05:15 +0530

    comman RGBA define

--- ios/RNJWPlayerNativeView.m
+++ ios/RNJWPlayerNativeView.m
@@ -46,6 +46,8 @@ NSString* const AudioInterruptionsEnded = @"AudioInterruptionsEnded";
 {
     JWConfig *config = [JWConfig new];
 
+#define RGBA(r, g, b, a) [UIColor colorWithRed:r/255.0 green: g/255.0 blue:b/255.0 alpha:a]
+
     //config.skin = JWPremiumSkinSeven;
     //config.preload = JWPreloadNone;
     config.stretching = JWStretchingUniform;
