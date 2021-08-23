bdb4a7b8b244643ed13f931fa24c60390041d2ea
Chaim Paneth <caapaneth@gmail.com>
2021-08-23 22:56:15 +0300

    App.js

---
+++ Example/App.js
@@ -0,0 +1,124 @@
+import React, {Component} from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Dimensions,
+  Platform,
+  StatusBar,
+} from 'react-native';
+
+import JWPlayer from 'react-native-jw-media-player/JWPlayerViewController';
+
+const {width} = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  subContainer: {
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
+  playerContainer: {
+    height: 300,
+    width: width - 40,
+  },
+  player: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 18,
+    margin: 40,
+  },
+});
+
+export default class App extends Component {
+  componentDidMount() {
+    // setTimeout(() => {
+    //   this.JWPlayer.togglePIP();
+    // }, 1000);
+  }
+
+  onBeforePlay() {
+    // eslint-disable-line
+    // console.log('onBeforePlay was called');
+  }
+
+  onPlay() {
+    // eslint-disable-line
+    // console.log('onPlay was called');
+  }
+
+  onPlayerError(error) {
+    // eslint-disable-line
+    console.log('onPlayerError was called with error: ', error);
+  }
+
+  onSetupPlayerError(error) {
+    // eslint-disable-line
+    console.log('onSetupPlayerError was called with error: ', error);
+  }
+
+  onBuffer() {
+    // eslint-disable-line
+    // console.log('onBuffer was called');
+  }
+
+  onTime({position, duration}) {
+    // eslint-disable-line
+    // console.log('onTime was called with: ', position, duration);
+  }
+
+  onFullScreen() {
+    StatusBar.setHidden(true);
+  }
+
+  onFullScreenExit() {
+    StatusBar.setHidden(false);
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.subContainer}>
+          <View style={styles.playerContainer}>
+            <JWPlayer
+              ref={p => (this.JWPlayer = p)}
+              style={styles.player}
+              config={{
+                license:
+                  Platform.OS === 'android'
+                    ? 'ANDROID_LICENSE'
+                    : 'IOS_LICENSE',
+                backgroundAudioEnabled: true,
+                autostart: true,
+                items: [
+                  {
+                    file: 'https://content.jwplatform.com/manifests/MEDIAID.m3u8',
+                  },
+                ],
+                playerSize: {
+                  width,
+                  height: 300,
+                },
+                nativeFullScreen: true,
+              }}
+              onBeforePlay={() => this.onBeforePlay()}
+              onPlay={() => this.onPlay()}
+              onSetupPlayerError={e => this.onSetupPlayerError(e)}
+              onPlayerError={e => this.onPlayerError(e)}
+              onBuffer={() => this.onBuffer()}
+              onTime={time => this.onTime(time)}
+              nativeFullScreen={true} // when undefined or false you will need to handle the player styles in onFullScreen & onFullScreenExit callbacks
+              onFullScreen={() => this.onFullScreen()}
+              onFullScreenExit={() => this.onFullScreenExit()}
+            />
+          </View>
+        </View>
+        <Text style={styles.text}>Welcome to react-native-jw-media-player</Text>
+      </SafeAreaView>
+    );
+  }
+}
