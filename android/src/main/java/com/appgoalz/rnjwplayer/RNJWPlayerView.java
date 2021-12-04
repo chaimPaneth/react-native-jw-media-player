1ca9c3fd5282e57440ad8e1ced1ba937ec115f7f
Chaim Paneth <caapaneth@gmail.com>
2021-12-04 20:16:56 +0200

    change prop in callback to visible

--- android/src/main/java/com/appgoalz/rnjwplayer/RNJWPlayerView.java
+++ android/src/main/java/com/appgoalz/rnjwplayer/RNJWPlayerView.java
@@ -860,7 +860,7 @@ public class RNJWPlayerView extends RelativeLayout implements
     public void onControlBarVisibilityChanged(ControlBarVisibilityEvent controlBarVisibilityEvent) {
         WritableMap event = Arguments.createMap();
         event.putString("message", "onControlBarVisible");
-        event.putBoolean("controls", controlBarVisibilityEvent.isVisible());
+        event.putBoolean("visible", controlBarVisibilityEvent.isVisible());
         getReactContext().getJSModule(RCTEventEmitter.class).receiveEvent(getId(), "topControlBarVisible", event);
 
         updateWakeLock(true);
