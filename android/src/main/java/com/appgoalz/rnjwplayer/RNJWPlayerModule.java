5646ff8e5a6db938f2b382bfd088fa080a0b7073
chaimPaneth <caapaneth@gmail.com>
2023-07-10 12:14:04 +0300

    remove faulty code

--- android/src/main/java/com/appgoalz/rnjwplayer/RNJWPlayerModule.java
+++ android/src/main/java/com/appgoalz/rnjwplayer/RNJWPlayerModule.java
@@ -65,7 +65,6 @@ public class RNJWPlayerModule extends ReactContextBaseJavaModule {
                     .stretching(oldConfig.getStretching())
                     .uiConfig(oldConfig.getUiConfig())
                     .playlist(createPlaylist(playlistItems))
-                    .sharingConfig(oldConfig.getSharingConfig())
                     .allowCrossProtocolRedirects(oldConfig.getAllowCrossProtocolRedirects())
                     .preload(oldConfig.getPreload())
                     .useTextureView(oldConfig.useTextureView())
